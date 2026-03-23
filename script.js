e778211241a774f517ad76188b991dafb8fd38aa
VaibhavC-Btech <vaibhavchekka8@gmail.com>
2026-03-23 14:01:00 +0530

    created files and also added basic layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
